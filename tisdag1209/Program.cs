b691b613d8b90f1754e25ad732756568be63fe41
shady9090 <sh-1990@hotmail.com>
2017-09-12 09:47:11 +0200

    Add project files.

---
+++ tisdag1209/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tisdag1209
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
